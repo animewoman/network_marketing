497495a61405536f84d420e084157c1fd59127c9
Andrei <andrew.misiyrin@gmail.com>
2021-05-26 20:55:53 +0600

    initial commit

---
+++ front-end/src/components/HelloWorld.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="hello">
+    <h1>{{ message }}</h1>
+    <q-btn no-caps>{{ msg }}</q-btn>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class HelloWorld extends Vue {
+  @Prop({ type: String, default: '' }) readonly msg!: string;
+
+  message = 'Hello World!';
+}
+</script>

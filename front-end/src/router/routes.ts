4ea592dfef99d6ee2447620ac635f786bd2b22e7
Andrei <andrew.misiyrin@gmail.com>
2021-05-27 20:30:11 +0600

    chore: Удалил тестовые файлы

--- front-end/src/router/routes.ts
+++ front-end/src/router/routes.ts
@@ -1,13 +1,7 @@
 import { RouteConfig } from 'vue-router';
-import Home from '@/pages/Home.vue';
 import Administration from '@/components/Administration.vue';
 
 const routes: RouteConfig[] = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home,
-  },
   {
     path: '/admin',
     name: 'Admin',

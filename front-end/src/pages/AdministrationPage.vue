28e96fcfc51c13621b2e8001bd22e41abc153f42
Andrei <andrew.misiyrin@gmail.com>
2021-05-29 22:06:20 +0600

    refactor: Перенес методы

--- front-end/src/pages/AdministrationPage.vue
+++ front-end/src/pages/AdministrationPage.vue
@@ -1,7 +1,7 @@
 <template>
-  <div>
-    <registration />
-    <users />
+  <div class="row container">
+    <registration @save-user="saveUser($event)" />
+    <users :users="users" :loading="loading" @delete-user="deleteUser($event)" />
   </div>
 </template>
 
@@ -9,12 +9,64 @@
 import { Component, Vue } from 'vue-property-decorator';
 import Registration from '@/components/Registration.vue';
 import Users from '@/components/Users.vue';
+import { getUsers, deleteUser, saveUser } from '@/service/Users';
+import { User } from '@/types/user';
 
 @Component({
   name: 'AdministrationPage',
   components: { Users, Registration },
 })
-export default class AdministrationPage extends Vue {}
+export default class AdministrationPage extends Vue {
+  users: User[] = [];
+  loading = false;
+
+  created() {
+    this.getUsers();
+  }
+
+  async getUsers() {
+    try {
+      this.loading = true;
+      this.users = await getUsers();
+    } catch (e) {
+      throw new Error(e);
+    } finally {
+      this.loading = false;
+    }
+  }
+
+  async deleteUser(user: User) {
+    try {
+      if (!user._id) {
+        return;
+      }
+
+      this.loading = true;
+      await deleteUser(user._id);
+      await this.getUsers();
+    } catch (e) {
+      throw new Error(e);
+    } finally {
+      this.loading = false;
+    }
+  }
+
+  async saveUser(user: User) {
+    try {
+      this.loading = true;
+      await saveUser(user);
+      await this.getUsers();
+    } catch (e) {
+      throw new Error(e);
+    } finally {
+      this.loading = false;
+    }
+  }
+}
 </script>
 
-<style scoped></style>
+<style scoped>
+.container {
+  justify-content: center;
+}
+</style>

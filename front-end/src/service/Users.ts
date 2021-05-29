df7aeb36562fbf4ee3e78072437269ed3b05c4ad
Andrei <andrew.misiyrin@gmail.com>
2021-05-29 22:05:25 +0600

    feat: Удаление пользователя

--- front-end/src/service/Users.ts
+++ front-end/src/service/Users.ts
@@ -1,14 +1,23 @@
 import { User } from '@/types/user';
 import axios from 'axios';
 
+const server = 'http://194.67.105.44:7777';
+
 export async function getUsers(): Promise<User[] | []> {
-  const response = await axios.get('http://194.67.105.44:7777/api/admin/main');
+  const response = await axios.get(`${server}/api/admin/main`);
 
   return response.data.users;
 }
 
 export async function saveUser(user: User): Promise<string> {
-  const response = await axios.post('http://194.67.105.44:7777/api/user/create', user);
+  const response = await axios.post(`${server}/api/user/create`, user);
 
   return response.data;
+}
+
+export async function deleteUser(id: string): Promise<User[] | []> {
+  const data = { _id: id };
+  const response = await axios.post(`${server}/api/user/delete`, data);
+
+  return response.data.data;
 }

f147822fb9497efbc41df1e6617d7e82d778f2e5
Andrei <andrew.misiyrin@gmail.com>
2021-05-31 19:03:41 +0600

    feat: Реализовал компоненту контроля пользователя

---
+++ front-end/src/components/Administration/UserControl.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="m-auto q-pt-xl" style="width: 600px">
+    <q-toolbar-title style="text-align: center">Пользователь {{ user.login }}</q-toolbar-title>
+    <q-form greedy>
+      <q-input label="ФИО пользователя" v-model="user.login" />
+      <q-input label="Почта" v-model="user.email" />
+      <q-input label="Телфеон" v-model="user.phone" />
+      <q-input label="Спонсор" v-model="user.parent" />
+      <q-input label="Баллы" v-model="user.money" />
+    </q-form>
+
+    <q-btn label="На таблицу пользователей" color="primary" no-caps class="q-my-lg" />
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import { User } from '@/types/user';
+
+@Component({
+  name: 'UserControl',
+})
+export default class UserControl extends Vue {
+  user: User = {
+    login: '',
+    phone: '',
+    email: '',
+    money: 0,
+  };
+}
+</script>
+
+<style scoped></style>
--- front-end/src/router/routes.ts
+++ front-end/src/router/routes.ts
@@ -1,11 +1,17 @@
 import { RouteConfig } from 'vue-router';
 import AdministrationPage from '@/pages/AdministrationPage.vue';
+import UserControl from '@/components/Administration/UserControl.vue';
 
 const routes: RouteConfig[] = [
   {
     path: '/admin',
-    name: 'Admin',
+    name: 'admin',
     component: AdministrationPage,
+  },
+  {
+    path: '/admin/user',
+    name: 'user-control',
+    component: UserControl,
   },
 ];
 

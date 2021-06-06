0835a01d4476c69440d36507ffbd3a27ccc0424d
Andrei <andrew.misiyrin@gmail.com>
2021-06-06 18:31:39 +0600

    chore: Мок компонент

---
+++ front-end/src/components/User/Partners.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>Partners</div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component({
+  name: 'Partners',
+})
+export default class Partners extends Vue {}
+</script>
+
+<style scoped></style>
---
+++ front-end/src/components/User/Reports.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>Reports</div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component({
+  name: 'Reports',
+})
+export default class Reports extends Vue {}
+</script>
+
+<style scoped></style>

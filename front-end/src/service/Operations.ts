d41edc562912a4a8c9420251c4d85173db64799e
Andrei <andrew.misiyrin@gmail.com>
2021-06-06 13:15:11 +0600

    refactor: Поправил путь к endpoint

--- front-end/src/service/Operations.ts
+++ front-end/src/service/Operations.ts
@@ -2,7 +2,7 @@ import { api } from '@/http';
 import { TransferData } from '@/types/operation';
 
 export async function transferMoney(data: TransferData): Promise<string> {
-  const response = await api().post('/transfer', data);
+  const response = await api().post('/user/send-money', data);
 
   console.log(response);
   return 'success';

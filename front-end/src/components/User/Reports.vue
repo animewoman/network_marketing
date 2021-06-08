9a4d71ad2f8fc65005c2730a73e004afd50367e2
Andrei <andrew.misiyrin@gmail.com>
2021-06-09 02:20:15 +0600

    feat: Реализовал компоненту 'Отчеты'

--- front-end/src/components/User/Reports.vue
+++ front-end/src/components/User/Reports.vue
@@ -1,14 +1,65 @@
 <template>
-  <div>Reports</div>
+  <div>
+    <q-table title="Отчеты" :columns="columns" :data="reports" :loading="loading" />
+  </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
+import { getReports } from '@/service/Operations';
 
 @Component({
   name: 'Reports',
 })
-export default class Reports extends Vue {}
+export default class Reports extends Vue {
+  loading = false;
+
+  reports = [];
+
+  columns = [
+    {
+      name: 'type',
+      label: 'Тип транзакции',
+      field: 'type',
+      align: 'left',
+      sortable: true,
+    },
+    {
+      name: 'score',
+      label: 'Баллы',
+      field: 'score',
+      align: 'left',
+      sortable: true,
+    },
+    {
+      name: 'from',
+      label: 'Отправитель/Получатель',
+      field: 'from',
+      align: 'right',
+      sortable: true,
+    },
+  ];
+
+  get userLogin(): string {
+    const login = localStorage.getItem('login');
+    return login!;
+  }
+
+  created() {
+    this.fetchReports();
+  }
+
+  async fetchReports() {
+    try {
+      this.loading = true;
+      this.reports = await getReports(this.userLogin);
+    } catch (e) {
+      console.log(e.message);
+    } finally {
+      this.loading = false;
+    }
+  }
+}
 </script>
 
 <style scoped></style>

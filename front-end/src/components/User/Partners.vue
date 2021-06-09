218619dab2807dcc0ca869350d0f04e773a77a5a
Andrei <andrew.misiyrin@gmail.com>
2021-06-09 23:43:04 +0600

    chore: Поправил верстку

--- front-end/src/components/User/Partners.vue
+++ front-end/src/components/User/Partners.vue
@@ -1,8 +1,41 @@
 <template>
   <div>
     <q-expansion-item
-      label="Первый этап"
-      class="shadow-1 overflow-hidden"
+      class="shadow-1 overflow-hidden q-my-sm"
+      style="border-radius: 30px"
+      header-class="bg-primary text-white"
+      expand-icon-class="text-white"
+    >
+      <template #header>
+        <q-toolbar-title> Первый этап </q-toolbar-title>
+      </template>
+
+      <div class="row q-pa-sm">
+        <q-card v-for="item in firstLine" :key="item" class="col-sm-3 q-ma-sm q-px-md">
+          <div class="self-center full-width no-outline" tabindex="0">
+            <div class="row">
+              <div class="q-pt-sm">
+                <p class="small-text">Логин</p>
+                <p class="small-text">Спонсор</p>
+              </div>
+
+              <div class="q-ml-lg">
+                <p>jhon234</p>
+                <p>boris</p>
+              </div>
+            </div>
+
+            <div class="row">
+              <q-space />
+              <q-btn label="Подробнее" icon="info" size="sm" color="primary" flat @click="showDetail = true" />
+            </div>
+          </div>
+        </q-card>
+      </div>
+    </q-expansion-item>
+
+    <q-expansion-item
+      class="shadow-1 overflow-hidden q-my-sm"
       style="border-radius: 30px"
       header-class="bg-primary text-white"
       expand-icon-class="text-white"
@@ -10,20 +43,20 @@
     >
       <template #header>
         <q-toolbar-title>
-          Первый этап <span style="font-size: 11px" class="q-ml-xl">{{ subtitle }}</span>
+          Второй этап <span style="font-size: 11px" class="q-ml-xl">{{ subtitle }}</span>
         </q-toolbar-title>
       </template>
 
       <div class="row q-pa-sm">
-        <q-card v-for="item in items" :key="item" class="col-sm-3 q-ma-sm q-px-md">
+        <q-card v-for="item in secondLine" :key="item" class="col-sm-3 q-ma-sm q-px-md">
           <div class="self-center full-width no-outline" tabindex="0">
             <div class="row">
-              <div class="q-mr-lg">
-                <p>Логин</p>
-                <p>Спонсор</p>
+              <div class="q-pt-sm">
+                <p class="small-text">Логин</p>
+                <p class="small-text">Спонсор</p>
               </div>
 
-              <div>
+              <div class="q-ml-lg">
                 <p>jhon234</p>
                 <p>boris</p>
               </div>
@@ -91,18 +124,24 @@ import { Component, Vue } from 'vue-property-decorator';
   name: 'Partners',
 })
 export default class Partners extends Vue {
-  items = [0, 1, 2];
+  firstLine = [1, 2];
+
+  secondLine = [1, 2, 3, 4];
 
   showDetail = false;
 
   get subtitle() {
     // только для активного этапа или отображать в другом месте
-    return `Партнеров до перехода в следующий этап  ${this.items[1]} / ${this.items[2]}`;
+    return `Партнеров до перехода в следующий этап  ${this.secondLine[2]} / ${this.secondLine[3]}`;
   }
 }
 </script>
 
 <style>
+.small-text {
+  font-size: 11px;
+}
+
 .background-title {
   text-align: center;
   background-color: rgba(82, 81, 81, 0.82);

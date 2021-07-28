fc5d65571632974cb75dea67528a99190ba22faf
Andrew Misyurin <andrew.misiyrin@gmail.com>
2021-07-28 21:17:34 +0600

    refactor: Правки по верстке

--- front-end/src/components/User/Partners.vue
+++ front-end/src/components/User/Partners.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <q-toolbar-title class="q-pb-lg q-pa-none">Структура</q-toolbar-title>
-    <div class="col bg-red justify-center">
+    <div class="col bg-red justify-center" style="height: 600px">
       <div v-if="stageNumber < 5" class="row justify-center q-mb-sm">
         <q-btn v-if="currentStage" label="Вы купили эликсир" color="green-5" text-color="black" no-caps />
 
@@ -38,153 +38,90 @@
       </div>
 
       <div v-if="currentStage && currentStage.length">
-        <div class="row justify-center">
-          <q-card v-if="currentStage[0]" class="col-2 q-ma-sm q-px-md bg-green-5">
-            <div>
-              <div class="row justify-center">
-                <p>{{ currentStage[0].login }}</p>
-              </div>
-
-              <div>
-                <q-btn
-                  label="Подробнее"
-                  icon="info"
-                  size="sm"
-                  color="primary"
-                  no-caps
-                  flat
-                  @click="showUserDetail(currentStage[0])"
-                />
-              </div>
-            </div>
+        <div class="row justify-center" style="height: 150px">
+          <q-card v-if="currentStage[0]" class="col-2 q-ma-sm bg-green-5" style="border-radius: 50%">
+            <q-item
+              class="row justify-center q-px-md"
+              clickable
+              @click="showUserDetail(currentStage[0])"
+              style="height: 100%"
+            >
+              <p>{{ currentStage[0].login }}</p>
+            </q-item>
           </q-card>
         </div>
 
         <q-separator />
 
-        <div class="row q-pt-sm" style="justify-content: center">
-          <q-card v-if="currentStage[1]" class="col-2 q-ma-sm q-px-md bg-green-5">
-            <div class="self-center full-width no-outline" tabindex="0">
-              <div class="row justify-center">
-                <p>{{ currentStage[1].login }}</p>
-              </div>
-
-              <div>
-                <q-btn
-                  label="Подробнее"
-                  icon="info"
-                  size="sm"
-                  color="primary"
-                  flat
-                  no-caps
-                  @click="showUserDetail(currentStage[1])"
-                />
-              </div>
-            </div>
+        <div class="row q-pt-sm justify-evenly" style="height: 150px">
+          <q-card v-if="currentStage[1]" class="col-2 q-ma-sm bg-green-5" style="border-radius: 50%">
+            <q-item
+              class="row justify-center q-px-md"
+              clickable
+              @click="showUserDetail(currentStage[1])"
+              style="height: 100%"
+            >
+              <p>{{ currentStage[1].login }}</p>
+            </q-item>
           </q-card>
 
-          <q-card v-if="currentStage[2]" class="col-2 q-ma-sm q-px-md bg-green-5">
-            <div class="self-center full-width no-outline" tabindex="0">
-              <div class="row justify-center">
-                <p>{{ currentStage[2].login }}</p>
-              </div>
-
-              <div>
-                <q-btn
-                  label="Подробнее"
-                  icon="info"
-                  size="sm"
-                  color="primary"
-                  flat
-                  no-caps
-                  @click="showUserDetail(currentStage[2])"
-                />
-              </div>
-            </div>
+          <q-card v-if="currentStage[2]" class="col-2 q-ma-sm bg-green-5" style="border-radius: 50%">
+            <q-item
+              class="row justify-center q-px-md"
+              clickable
+              @click="showUserDetail(currentStage[2])"
+              style="height: 100%"
+            >
+              <p>{{ currentStage[2].login }}</p>
+            </q-item>
           </q-card>
         </div>
 
         <q-separator />
 
-        <div class="row q-pt-sm" style="justify-content: center">
-          <q-card v-if="currentStage[3]" class="col-2 q-ma-sm q-px-md bg-green-5">
-            <div class="self-center full-width no-outline" tabindex="0">
-              <div class="row justify-center">
-                <p>{{ currentStage[3].login }}</p>
-              </div>
-
-              <div>
-                <q-btn
-                  label="Подробнее"
-                  icon="info"
-                  size="sm"
-                  color="primary"
-                  flat
-                  no-caps
-                  @click="showUserDetail(currentStage[3])"
-                />
-              </div>
-            </div>
+        <div class="row q-pt-sm justify-evenly" style="height: 150px">
+          <q-card v-if="currentStage[3]" class="col-2 q-ma-sm bg-green-5" style="border-radius: 50%">
+            <q-item
+              class="row justify-center q-px-md"
+              clickable
+              @click="showUserDetail(currentStage[3])"
+              style="height: 100%"
+            >
+              <p>{{ currentStage[3].login }}</p>
+            </q-item>
           </q-card>
 
-          <q-card v-if="currentStage[4]" class="col-2 q-ma-sm q-px-md bg-green-5">
-            <div class="self-center full-width no-outline" tabindex="0">
-              <div class="row justify-center">
-                <p>{{ currentStage[4].login }}</p>
-              </div>
-
-              <div>
-                <q-btn
-                  label="Подробнее"
-                  icon="info"
-                  size="sm"
-                  color="primary"
-                  flat
-                  no-caps
-                  @click="showUserDetail(currentStage[4])"
-                />
-              </div>
-            </div>
+          <q-card v-if="currentStage[4]" class="col-2 q-ma-sm bg-green-5" style="border-radius: 50%">
+            <q-item
+              class="row justify-center q-px-md"
+              clickable
+              @click="showUserDetail(currentStage[4])"
+              style="height: 100%"
+            >
+              <p>{{ currentStage[4].login }}</p>
+            </q-item>
           </q-card>
 
-          <q-card v-if="currentStage[5]" class="col-2 q-ma-sm q-px-md bg-green-5">
-            <div class="self-center full-width no-outline" tabindex="0">
-              <div class="row justify-center">
-                <p>{{ currentStage[5].login }}</p>
-              </div>
-
-              <div>
-                <q-btn
-                  label="Подробнее"
-                  icon="info"
-                  size="sm"
-                  color="primary"
-                  flat
-                  no-caps
-                  @click="showUserDetail(currentStage[5])"
-                />
-              </div>
-            </div>
+          <q-card v-if="currentStage[5]" class="col-2 q-ma-sm bg-green-5" style="border-radius: 50%">
+            <q-item
+              class="row justify-center q-px-md"
+              clickable
+              @click="showUserDetail(currentStage[5])"
+              style="height: 100%"
+            >
+              <p>{{ currentStage[5].login }}</p>
+            </q-item>
           </q-card>
 
-          <q-card v-if="currentStage[6]" class="col-2 q-ma-sm q-px-md bg-green-5">
-            <div class="self-center full-width no-outline" tabindex="0">
-              <div class="row justify-center">
-                <p>{{ currentStage[6].login }}</p>
-              </div>
-
-              <div>
-                <q-btn
-                  label="Подробнее"
-                  icon="info"
-                  size="sm"
-                  color="primary"
-                  flat
-                  no-caps
-                  @click="showUserDetail(currentStage[6])"
-                />
-              </div>
-            </div>
+          <q-card v-if="currentStage[6]" class="col-2 q-ma-sm bg-green-5" style="border-radius: 50%">
+            <q-item
+              class="row justify-center q-px-md"
+              clickable
+              @click="showUserDetail(currentStage[6])"
+              style="height: 100%"
+            >
+              <p>{{ currentStage[6].login }}</p>
+            </q-item>
           </q-card>
         </div>
       </div>
